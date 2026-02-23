9dba6527d7cb1a9916dd306b96686fdf481862bd
surayaafroze63 <surayaafroze63@gmail.com>
2026-02-23 08:27:31 +0600

    add filter section

--- script/script.js
+++ script/script.js
@@ -11,6 +11,9 @@ let rejectedCount=document.getElementById('rejectedCount')//(header part)
 
 let allCards=document.getElementById('allCards')//card section
 
+let filteredSection=document.getElementById('filterSection')//faka section jeta nea hoyece
+
+
 
 ///card info show er jonno main container k dhora hoyece karon er moddhe sob info ache
 let maincontainer=document.querySelector('main');
@@ -64,29 +67,65 @@ selected.classList.add('bg-[#3B82F6]','text-white')
 }
 
 ///card info show
-maincontainer.addEventListener('click',function(event){
-  const parentnode=event.target.parentNode.parentNode;
-  const mobileInfo=parentnode.querySelector('.mobileInfo').innerText
-  
-  const nativInfo=parentnode.querySelector('.nativInfo').innerText
-  
-  const paymentInfo=parentnode.querySelector('.paymentInfo').innerText
- 
-  const applyInfo=parentnode.querySelector('.applyInfo').innerText
-  
-  const buildPlatform=parentnode.querySelector('.buildPlatform').innerText
-  
+maincontainer.addEventListener('click', function(event){
+  if(event.target.classList.contains('inteviewBtn')){
+    const parentnode = event.target.closest('.box-1, .box-2, .box-3, .box-4, .box-5, .box-6, .box-7, .box-8');
+    console.log(parentnode);
 
- const cardInfo={mobileInfo,nativInfo,paymentInfo,applyInfo,buildPlatform}
-    
-  
-console.log(cardInfo)
-  const mobileInfoExist= interviewList.find(item=>item.mobileInfo==cardInfo.mobileInfo)
+    const mobileInfo = parentnode.querySelector('.mobileInfo').innerText;
+    const nativInfo = parentnode.querySelector('.nativInfo').innerText;
+    const paymentInfo = parentnode.querySelector('.paymentInfo').innerText;
+    const applyInfo = parentnode.querySelector('.applyInfo').innerText;
+    const buildPlatform = parentnode.querySelector('.buildPlatform').innerText;
 
-  if(!mobileInfoExist){
-    interviewList.push(cardInfo)
+    const cardInfo = {mobileInfo, nativInfo, paymentInfo, applyInfo, buildPlatform};
+    const mobileInfoExist = interviewList.find(item => item.mobileInfo == cardInfo.mobileInfo);
+
+    parentnode.querySelector('.applyInfo').innerText = 'Interview';
+
+    if(!mobileInfoExist){
+      interviewList.push(cardInfo)
+    }
+    renderInterview();
+    calculateTotalCount();
   }
-  
-})
+});
 
+
+
+//interview er jonno mane thrive click korle sekhane akta akta kore card jabe
+function renderInterview(){
+  filteredSection.innerHTML='';
+  for(let interview of interviewList){
+    let status = interview.applyInfo; // 
+
+    
+    let statusClass = status === 'Interview' ? 'bg-[#10B981] text-white' : 'bg-[#F1F2F4] text-black';
+
+    let div = document.createElement('div');
+    div.className='box-1 flex justify-between border-2 border-gray-300 py-5 px-6 rounded-[8px] bg-white';
+    div.innerHTML=`<div class="left space-y-6">
+        <div class="space-y-1.5">
+          <p class="mobileInfo font-bold text-[#002C5C]">${interview.mobileInfo}</p>
+          <p class="nativInfo text-[#64748B]">${interview.nativInfo}</p>
+        </div>
+        <div>
+          <p class="paymentInfo text-[#64748B]">${interview.paymentInfo}</p>
+        </div>
+        <div>
+          <p class="applyInfo ${statusClass} px-5 py-2 inline rounded-xl">${status}</p>
+          <p class="buildPlatform mt-3 text-[#323B49]">${interview.buildPlatform}</p>
+        </div>
+        <div class="flex gap-2">
+          <button class="inteviewBtn border-2 font-bold border-green-400 text-green-400 py-1 px-3.5 rounded-[4px]">Interview</button>
+          <button class="rejectBtn border-2 font-bold border-red-400 text-red-400 py-1 px-3.5 rounded-[4px]">Rejected</button>
+        </div>
+      </div>
+
+      <div class="right">
+        <img src="Trash.png" alt="">
+      </div>`;
+    filteredSection.appendChild(div);
+  }
+}
 
